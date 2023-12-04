0679f5877e29b9fea1ffd1af637b0659235322de
codershisho <codershisho@gmail.com>
2023-12-04 21:06:55 +0900

    seeder追加

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
         $this->call(StatusSeeder::class);
         $this->call(MCategorySeeder::class);
         $this->call(MenuTableSeeder::class);
+        $this->call(MScrapeSitesTableSeeder::class);
     }
 }
---
+++ backend/database/seeders/MScrapeSitesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\MScrapeSite;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MScrapeSitesTableSeeder extends Seeder
+{
+    public function run()
+    {
+        MScrapeSite::truncate();
+
+        $data = [
+            ['name' => 'RENCA', 'url' => 'https://renca.jp/', 'created_at' => now(), 'updated_at' => now()],
+        ];
+
+        MScrapeSite::insert($data);
+    }
+}
