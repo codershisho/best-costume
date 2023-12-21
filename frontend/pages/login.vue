a0c805b2facc84c1317d122f09d645cbf2c8276d
tk-kw <takatully123@gmail.com>
2023-12-21 21:06:22 +0900

    231221

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="d-flex justify-center align-center tw-h-screen">
     <div>
-      <div class="text-center tw-text-3xl tw-font-bold">STUDIO ODDO</div>
+      <div class="tw-w-60 tw-m-auto tw-mb-4">
+        <img src="oddo_logo.png">
+      </div>
       <div class="text-center tw-text-2xl tw-text-gray-500">
         Customer Costume Selection Application
       </div>
-      <div class="tw-mt-10 tw-text-xl">User Name</div>
-      <base-text v-model="credentials.name"></base-text>
-      <div class="tw-text-xl">Password</div>
-      <base-text v-model="credentials.password"></base-text>
+      <div class="tw-mb-4">
+        <div class="tw-mt-10 tw-text-xl tw-mb-2">User Name</div>
+        <base-text v-model="credentials.name"></base-text>
+      </div>
+      <div class="tw-mb-7">
+        <div class="tw-text-xl tw-mb-2">Password</div>
+        <base-text v-model="credentials.password"></base-text>
+      </div>
       <base-button
         class="w-100 !tw-h-12"
         color="blue-accent-3"
