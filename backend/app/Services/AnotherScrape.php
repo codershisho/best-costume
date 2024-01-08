a07cfb00245934a34d34716490c492d894047b0d
codershisho <codershisho@gmail.com>
2024-01-08 14:56:42 +0900

    京都かしいしょうの追加

--- backend/app/Services/AnotherScrape.php
+++ backend/app/Services/AnotherScrape.php
@@ -3,15 +3,28 @@
 namespace App\Services;
 
 use App\Models\TScrapeSite;
+use Illuminate\Support\Facades\Log;
 
 class AnotherScrape extends AbstractScrape
 {
-    public function scrape(): void
-    {
-    }
+    protected static $_siteId = 2;
 
-    public function store(): TScrapeSite
+    public function scrapeFilter(): void
     {
-        return new TScrapeSite();
+        $title = $this->_crawler->filter('.panel > h1');
+        $description = $this->_crawler->filter('div > div.panel-1 > table > tbody > tr:last-child > td');
+        $price = $this->_crawler->filter('span.kakaku');
+        $images = $this->_crawler->filter('li > a > img')->each(function ($el) {
+            return ($el->attr('src'));
+        });
+
+        $this->_entity = [
+            'site_id' => self::$_siteId,
+            'url' => $this->_siteUrl,
+            'title' => $title->text(),
+            'description' => $description->text(),
+            'price' => preg_replace('/[^0-9]/', '', $price->text()),
+            'images' => implode(',', $images)
+        ];
     }
 }
