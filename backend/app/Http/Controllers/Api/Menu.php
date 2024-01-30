6538818772690bfad49c743c87578464ef063b63
codershisho <codershisho@gmail.com>
2024-01-30 23:08:39 +0900

    close #52,#30,#24

--- backend/app/Http/Controllers/Api/Menu.php
+++ backend/app/Http/Controllers/Api/Menu.php
@@ -4,13 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\MMenu;
+use App\Models\MProduct;
 use Illuminate\Http\Request;
 
 class Menu extends Controller
 {
     public function index()
     {
-        $data = MMenu::with('children')->whereNull('parent_id')->get();
+        $data = MMenu::with(['children'])->whereNull('parent_id')->get();
+
+        $data = $data->map(function ($parent) {
+            $children = collect($parent['children']);
+            $parent['children'] = $children->map(function ($child) {
+                $count = MProduct::where('category_id', $child['id'])->count();
+                $child['count'] = $count;
+                return $child;
+            });
+            return $parent;
+        });
+
         return response()->json([
             'data' => $data,
             'message' => ''
